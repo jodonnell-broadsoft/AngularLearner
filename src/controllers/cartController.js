7d894950040e06e1e93d8976108dea1f2ba132c7
jonathanodonnell <jonathan.carl.odonnell@gmail.com>
2014-07-29 13:17:10 -0300

    Shopping Cart example part 3

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -1,26 +1,21 @@
 function CartController($scope) {
+    $scope.bill = {};
+
     $scope.items = [
         {title: 'Paint pots', quantity: 8, price: 3.26},
         {title: 'Paint', quantity: 10, price: 4.33},
         {title: 'Candy', quantity: 12, price: 7.22}
     ];
 
-    $scope.totalCart = function(){
+    $scope.$watch(function(){ //monitors any change
+        console.log('here');
         var total = 0;
         $scope.items.forEach(function(item){
             total = total + item.price * item.quantity;
         });
 
-        return total;
-    };
-
-    $scope.subtotal = function(){
-        return $scope.totalCart() - $scope.discount;
-    }
-
-    function calculateDiscount(newValue, oldValue, scope){
-        $scope.discount = newValue > 100 ? 10 : 0;
-    }
-
-    $scope.$watch($scope.totalCart, calculateDiscount);
+        $scope.bill.total = total;
+        $scope.bill.discount = total > 100 ? 10 : 0;
+        $scope.bill.subtotal = total - $scope.bill.discount;
+    });
 }
