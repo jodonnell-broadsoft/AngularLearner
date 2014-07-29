de9055407a65457332047aa2b9d8b2c63967cfea
jonathanodonnell <jonathan.carl.odonnell@gmail.com>
2014-07-29 09:56:06 -0300

    Got the shopping cart example workin

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,3 +1,11 @@
-function HelloController($scope) {
-    $scope.greeting = { text: 'Hello' };
+function CartController($scope) {
+    $scope.items = [
+        {title: 'Paint pots', quantity: 8, price: 3.26},
+        {title: 'Paint', quantity: 10, price: 4.33},
+        {title: 'Candy', quantity: 12, price: 7.22}
+    ];
+
+    $scope.remove = function(index){
+        $scope.items.splice(index, 1);
+    }
 }
