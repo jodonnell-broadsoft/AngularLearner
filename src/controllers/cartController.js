d7b2dc23bac34affffc27c1a4085038908b5e481
jonathanodonnell <jonathan.carl.odonnell@gmail.com>
2014-07-30 15:48:16 -0300

    Adding my first factory

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -4,13 +4,13 @@ function CartController($scope, Items) {
     $scope.bill = {};
 
     $scope.items = Items;
-  console.log(Items);
+    console.log(Items);
 
     $scope.$watch(function(){ //monitors any change
         console.log('here');
         var total = 0;
         $scope.items.forEach(function(item){
-            total = total + item.price * item.quantity;
+            total = item.price;
         });
 
         $scope.bill.total = total;
@@ -18,15 +18,3 @@ function CartController($scope, Items) {
         $scope.bill.subtotal = total - $scope.bill.discount;
     });
 }
-
-app.factory('Items', function() {
-  var items = {};
-  items.query = function() {
-    // In real apps, we'd pull this data from the server...
-    return [
-      {title: 'Paint pots', description: 'Pots full of paint', price: 3.95},
-      {title: 'Polka dots', description: 'Dots with polka', price: 2.95},
-      {title: 'Pebbles', description: 'Just little rocks', price: 6.95}
-    ]; };
-  return items;
-});
---
+++ src/factories/itemsFactory.js
@@ -0,0 +1,7 @@
+app.factory('Items', function() {
+  return [
+    {title: 'Paint pots', description: 'Pots full of paint', price: 3.95},
+    {title: 'Polka dots', description: 'Dots with polka', price: 2.95},
+    {title: 'Pebbles', description: 'Just little rocks', price: 6.95}
+  ];
+});
